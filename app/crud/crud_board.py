0b31a85bbc27d2f63dc76817d4ce0d1e6abfefd2
leegusrb <jeffsep24@gmail.com>
2026-02-03 16:18:04 +0900

    [Feat] Board CRUD 생성

--- app/crud/crud_board.py
+++ app/crud/crud_board.py
@@ -1,35 +1,38 @@
-from typing import List, Optional
 from sqlalchemy.orm import Session
 
-from app.crud.base import CRUDBase
 from app.models.board import Board
 from app.schemas.board import BoardCreate, BoardUpdate
 
 
-class CRUDBoard(CRUDBase[Board, BoardCreate, BoardUpdate]):
+def get_board_list(db: Session, skip: int = 0, limit: int = 10):
+    _board_list = db.query(Board).order_by(Board.created_at.desc())
 
-    # 게시글 생성 (작성자 ID 포함)
-    def create_with_owner(
-            self, db: Session, *, obj_in: BoardCreate, user_id: int
-    ) -> Board:
-        obj_in_data = obj_in.model_dump()
-        db_obj = Board(**obj_in_data, user_id=user_id)
-        db.add(db_obj)
-        db.commit()
-        db.refresh(db_obj)
-        return db_obj
+    total = _board_list.count()
+    board_list = _board_list.offset(skip).limit(limit).all()
 
-    # [수정됨] 목록 조회 (페이지네이션 지원용)
-    # Router에서 page를 받아서 skip = (page - 1) * limit 계산 후 넘겨주면 됩니다.
-    def get_multi_with_owner_check(
-            self, db: Session, *, skip: int = 0, limit: int = 10, user_id: Optional[int] = None
-    ) -> List[Board]:
-        query = db.query(self.model)
-        if user_id:
-            query = query.filter(Board.user_id == user_id)
-
-        # 최신글 순으로 정렬 (내림차순)
-        return query.order_by(Board.id.desc()).offset(skip).limit(limit).all()
+    return total, board_list
 
 
-board = CRUDBoard(Board)
+def get_board(db: Session, board_id: int):
+    board = db.query(Board).filter(Board.id == board_id).first()
+
+    return board
+
+def create_board(db: Session, board_create: BoardCreate):
+    db_board = Board(
+        title=board_create.title,
+        content=board_create.content,
+        category=board_create.category
+    )
+    db.add(db_board)
+    db.commit()
+
+def update_board(db: Session, db_board: Board, board_update: BoardUpdate):
+    db_board.title = board_update.title
+    db_board.content = board_update.content
+    db.add(db_board)
+    db.commit()
+
+def delete_board(db: Session, db_board: Board):
+    db.delete(db_board)
+    db.commit()
