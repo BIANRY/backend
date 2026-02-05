e091fd17f7e5626fc4bfa5bb6118aed1004ae4bd
leegusrb <jeffsep24@gmail.com>
2026-02-05 20:55:33 +0900

    [Feat] 앱 기본 설정 변경

--- app/core/config.py
+++ app/core/config.py
@@ -2,14 +2,14 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     # 기본 설정
-    PROJECT_NAME: str = "My FastAPI Project"
+    PROJECT_NAME: str = "BINARY homepage"
     API_V1_STR: str = "/api/v1"
 
     # 데이터베이스 설정 (기본값 설정 또는 필수값 지정)
     DATABASE_URL: str = "sqlite:///./sql_app.db"
 
     # 보안 관련 (JWT Secret Key 등)
-    SECRET_KEY: str = "YOUR_SUPER_SECRET_KEY"
+    SECRET_KEY: str = "SECRET_KEY"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
