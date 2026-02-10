54a4c70b2c0dee3bd10541b44aa0bc73665c8a3a
leegusrb <jeffsep24@gmail.com>
2026-02-10 17:23:02 +0900

    [Feat] Board API 연결

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import users
+from app.api.v1.endpoints import users, boards
 
 api_router = APIRouter()
 
@@ -8,4 +8,10 @@ api_router.include_router(
     users.router,
     prefix="/users",
     tags=["users"]
+)
+
+api_router.include_router(
+    boards.router,
+    prefix="/boards",
+    tags=["boards"]
 )
--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.api.deps import get_db
+from app.models.user import User
 
 router = APIRouter()
 
-@router.get("/users/")
+@router.get("/")
 def read_users(db: Session = Depends(get_db)):
     # db 변수는 이제 MySQL과 연결된 세션 객체입니다.
-    # users = db.query(User).all()
-    # return users
-    return {"message": "MySQL DB connected!"}
+    users = db.query(User).all()
+    return users
