3750d147e0d15f83d7d7f069792bcb323ed0ad0c
leegusrb <jeffsep24@gmail.com>
2026-02-26 16:53:03 +0900

    [Feat] 게시판 내용 타입 변경

--- app/models/board.py
+++ app/models/board.py
@@ -1,15 +1,14 @@
-from sqlalchemy import Column, Integer, String, DateTime, func, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, func, ForeignKey, Text
 from sqlalchemy.orm import relationship
 
 from app.db.base import Base
-
 
 class Board(Base):
     __tablename__ = "boards"
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String(255), nullable=False)
-    content = Column(String(255), nullable=False)
+    content = Column(Text, nullable=False)
     category = Column(String(255), nullable=False)
     created_at = Column(DateTime, server_default=func.now())
 
